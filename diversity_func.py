e8aa11981b09677ee913f2cdcfff4f7a871c9242
M-I-Dx <m.i.dxoo1@gmail.com>
2021-03-26 17:50:39 +0530

    Adding the .ckpt support

--- diversity_func.py
+++ diversity_func.py
@@ -1,11 +1,14 @@
 from da_utils import ModelUtils
 from da_algos import DaTechniques
+import sys
+sys.path.insert(0,"./Self-Supervised-Learner")
+from models import SIMCLR
 
 
 def get_diversity_imgs(DATA_PATH, DIVERSITY_DATA, MODEL_PATH, subset_size, 
                             img_size, embedding_size, technique, sample_size):
     utils = ModelUtils(MODEL_PATH, DATA_PATH, img_size, embedding_size)
-    filenames, feature_list = utils.get_embeddings()
+    filenames, feature_list = get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size)
     da = DaTechniques(subset_size, filenames, feature_list, sample_size)
 
     if technique=='DA_STD':
@@ -17,3 +20,29 @@ def get_diversity_imgs(DATA_PATH, DIVERSITY_DATA, MODEL_PATH, subset_size,
     utils.prepare_dataset(DIVERSITY_DATA)
     utils.plot_umap(DIVERSITY_DATA)
 
+
+
+def get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size):
+    def to_tensor(pil):
+        return torch.tensor(np.array(pil)).permute(2,0,1).float()
+    t = transforms.Compose([
+                            transforms.Resize((img_size, img_size)),
+                            transforms.Lambda(to_tensor)
+                            ])
+    dataset = ImageFolder(DATA_PATH, transform = t)
+    model = SIMCLR.SIMCLR.load_from_checkpoint(MODEL_PATH)
+    model.eval()
+    model.cuda()
+    with torch.no_grad():
+        data_matrix = torch.empty(embedding_size = (0, embedding_size)).cuda()
+        bs = 32
+        if len(dataset) < bs:
+          bs = 1
+        loader = DataLoader(dataset, batch_size = bs, shuffle = False)
+        for batch in tqdm(loader):
+            x = batch[0].cuda()
+            embeddings = model(x)
+            data_matrix = torch.vstack((data_matrix, embeddings))
+    paths = [dataset.imgs[i][0] for i in range(len(dataset.imgs))]
+    img_embeddings = data_matrix.cpu().detach().numpy()
+    return paths, img_embeddings
