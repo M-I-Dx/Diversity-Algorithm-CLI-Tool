8a5ce61c0e55f2d9c79fa2f6810b7c04287991bb
M-I-Dx <m.i.dxoo1@gmail.com>
2021-03-26 19:11:55 +0530

    Added the .ckpt support

--- diversity_func.py
+++ diversity_func.py
@@ -1,14 +1,26 @@
-from da_utils import ModelUtils
-from da_algos import DaTechniques
 import sys
 sys.path.insert(0,"./Self-Supervised-Learner")
-from models import SIMCLR
+
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+from sklearn.preprocessing import LabelEncoder
+
+from da_utils import ModelUtils
+from da_algos import DaTechniques
+
+from models import SIMCLR, SIMSIAM
 
 
 def get_diversity_imgs(DATA_PATH, DIVERSITY_DATA, MODEL_PATH, subset_size, 
-                            img_size, embedding_size, technique, sample_size):
+                            img_size, embedding_size, technique, sample_size, model_type="SIMCLR"):
     utils = ModelUtils(MODEL_PATH, DATA_PATH, img_size, embedding_size)
-    filenames, feature_list = get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size)
+    filenames, feature_list = get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size, model_type)
     da = DaTechniques(subset_size, filenames, feature_list, sample_size)
 
     if technique=='DA_STD':
@@ -18,11 +30,9 @@ def get_diversity_imgs(DATA_PATH, DIVERSITY_DATA, MODEL_PATH, subset_size,
     utils.filenames = da_files
     utils.embeddings = da_embeddings
     utils.prepare_dataset(DIVERSITY_DATA)
-    utils.plot_umap(DIVERSITY_DATA)
 
 
-
-def get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size):
+def get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size, model_type="SIMCLR"):
     def to_tensor(pil):
         return torch.tensor(np.array(pil)).permute(2,0,1).float()
     t = transforms.Compose([
@@ -30,11 +40,15 @@ def get_matrix(MODEL_PATH, DATA_PATH, img_size, embedding_size):
                             transforms.Lambda(to_tensor)
                             ])
     dataset = ImageFolder(DATA_PATH, transform = t)
-    model = SIMCLR.SIMCLR.load_from_checkpoint(MODEL_PATH)
+    if model_type = "SIMCLR":
+        model = SIMCLR.SIMCLR.load_from_checkpoint(MODEL_PATH, DATA_PATH)
+    else:
+        model = SIMSIAM.SIMSIAM.load_from_checkpoint(MODEL_PATH, DATA_PATH)
     model.eval()
     model.cuda()
+    size = embedding_size
     with torch.no_grad():
-        data_matrix = torch.empty(embedding_size = (0, embedding_size)).cuda()
+        data_matrix = torch.empty(size = (0, size)).cuda()
         bs = 32
         if len(dataset) < bs:
           bs = 1
